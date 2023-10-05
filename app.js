7aa3a27bdae58f18ec90b25d154dc804e58c736a
Ibrahim <75900839+ibrahim742@users.noreply.github.com>
2023-10-05 19:10:57 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Mengunci Scroll
 const rootElement = document.querySelector(":root");
+const audioIconWrapper = document.querySelector(".audio-icon-wrapper");
+const audioIcon = document.querySelector(".audio-icon-wrapper i");
+const song = document.querySelector("#song");
+let isPlaying = false;
 
 function disableScroll() {
   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -14,13 +18,43 @@ function disableScroll() {
 function enableScroll() {
   window.onscroll = function () {};
   rootElement.style.scrollBehavior = "smooth";
-  localStorage.setItem("opened", "true");
+  // localStorage.setItem("opened", "true");
+  playAudio();
 }
-if (!localStorage.getItem("opend")) {
-  disableScroll();
+
+function playAudio() {
+  song.volume = 0.1;
+  audioIconWrapper.style.display = "flex";
+  song.play();
+  isPlaying = true;
 }
+
+audioIconWrapper.onclick = function () {
+  if (isPlaying) {
+    song.pause();
+    audioIcon.classList.remove("bi-disc");
+    audioIcon.classList.add("bi-pause-circle");
+  } else {
+    song.play();
+    audioIcon.classList.add("bi-disc");
+    audioIcon.classList.remove("bi-pause-circle");
+  }
+
+  isPlaying = !isPlaying;
+};
+
+// if (!localStorage.getItem("opened")) {
+//   disableScroll();
+// }
 disableScroll();
 
+const urlParams = new URLSearchParams(window.location.search);
+const nama = urlParams.get("n") || "";
+const pronoun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
+const namaContainer = document.querySelector(".hero h4 span");
+namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, ",");
+
+document.querySelector("#nama").value = nama;
 
 // ============= header ===========
 const stickyTop = document.querySelector(".sticky-top");
